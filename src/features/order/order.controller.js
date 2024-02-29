08d851378713018816e28ca5b8799b18ca2f840d
codeonoto <onoto0409@gmail.com>
2024-02-29 22:18:26 +0530

    Transactions in MongoDB - 2

--- src/features/order/order.controller.js
+++ src/features/order/order.controller.js
@@ -7,7 +7,7 @@ export default class OrderController {
 
   async placeOrder(req, res, next) {
     try {
-      const userId = req.userId;
+      const userId = req.userID;
       await this.orderRepository.placeOrder(userId);
       res.status(201).send('Order is Created');
     } catch (err) {
--- src/features/order/order.repository.js
+++ src/features/order/order.repository.js
@@ -1,15 +1,54 @@
-export default class OrderRepository{
-    constructor(){
-        this.collection = "orders"
-    }
+import { ObjectId } from 'mongodb';
+import { getDB } from '../../config/mongodb.js';
 
-    async placeOrder(userId){
-        // 1. Get cardItems and calculate totalAmount.
+export default class OrderRepository {
+  constructor() {
+    this.collection = 'orders';
+  }
 
-        // 2. Create an order record.
+  async placeOrder(userId) {
+    // 1. Get cardItems and calculate totalAmount.
+    await this.getTotalAmount(userId);
+    // 2. Create an order record.
+    // 3. Reduce the stock.
+    // 4. Clear the card items.
+  }
 
-        // 3. Reduce the stock.
-
-        // 4. Clear the card items.
-    }
+  async getTotalAmount(userId) {
+    const db = getDB();
+    const items = await db
+      .collection('cartItems')
+      .aggregate([
+        // 1. Get cart items for the user
+        {
+          $match: { userID: new ObjectId(userId) },
+        },
+        // 2. Get the products from products collections
+        {
+          $lookup: {
+            from: 'products',
+            localField: 'productID',
+            foreignField: '_id',
+            as: 'productInfo',
+          },
+        },
+        // 3. Unwind the productInfo
+        {
+          $unwind: '$productInfo',
+        },
+        // 4. Calculate totalAmount for each cartItems.
+        {
+          $addFields: {
+            totalAmount: {
+              $multiply: ['$productInfo.price', '$quantity'],
+            },
+          },
+        },
+      ])
+      .toArray();
+    const finalTotalAmount = items.reduce(
+      (acc, item) => acc + item.totalAmount
+    );
+    console.log(finalTotalAmount);
+  }
 }
